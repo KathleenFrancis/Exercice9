c82ba13701d22da1774978c36e2348fb42e425a1
Kathleen Francis Mbo <e1410158@cmaisonneuve.qc.ca>
2019-03-22 19:35:53 -0400

    debut exercice 9

---
+++ js/animation.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+    console.log('oui animation');
+
+    let titre = document.querySelector('.site-title');
+    let sousTitre = document.querySelector('.site-description');
+
+    titre.classList.add("animTitre");
+    sousTitre.classList.add("animSousTitre");
+
+    let ps = document.querySelector('.ps');
+    ps.classList.add("animPS");
+
+    //console.log(titre);
+})
